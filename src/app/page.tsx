b9f2080ee1f07c4896977944e7ebd6c7189f30db
Abhishekyadav26 <aky732244@gmail.com>
2025-03-10 23:20:16 +0530

    Some ui changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { ConnectButton, ConnectEmbed, lightTheme } from "thirdweb/react";
+import { ConnectEmbed, lightTheme, useActiveAccount } from "thirdweb/react";
 import { Hero } from "./components/Hero";
 import { client } from "./client";
 import { defineChain } from "thirdweb";
 
-
 export default function Home() {
+  const account = useActiveAccount();
   return (
     <main>
-      <Hero />
+      <div className="flex items-center content-center h-screen justify-center">
+        <ConnectEmbed
+          client={client}
+          theme={lightTheme()}
+          chain={defineChain(57054)}
+        />
+        {account?.address ? <Hero /> : ""}
+      </div>
     </main>
   );
 }
